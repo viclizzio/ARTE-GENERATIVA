7d46831f9d9da19161d2abe15f6cbde6897b0e30
viclizzio <victorializzio@abacatania.it>
2024-05-15 18:05:50 +0200

    Update sketch.js

--- Esercizio 13/sketch.js
+++ Esercizio 13/sketch.js
@@ -1,10 +1,17 @@
+var y;
+var v=3;
+var dir=-1;
+
 function setup(){
   createCanvas(400,400);
-  
+  y=height/2;
 }
 
 function draw() {
-  background (0);
-
-  
+  background (0,0,0,10);
+  triangle (width/2,y, 0+20,height-20, width-20,height-20);
+  y+=v*dir;
+  if (y<0 || y>height-20){
+    dir=-dir;
+  }
 }
