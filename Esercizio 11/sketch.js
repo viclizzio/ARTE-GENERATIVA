679238ba7a5b08cd7b62244162f9b193cb843259
viclizzio <victorializzio@abacatania.it>
2024-05-15 17:09:21 +0200

    .

--- Esercizio 11/sketch.js
+++ Esercizio 11/sketch.js
@@ -1,17 +1,20 @@
-var x;
-var y;
+var x=0;
+var v=5;
 var dir=1;
 
-function setup() {
+function setup(){
   createCanvas(400,400);
+  strokeWeight(2
+  );
 }
 
 function draw() {
   background (0);
 
   stroke (255, 255, 0);
-  line (width/2,0,width/2,height);
-
-  stroke (0,0,255);
-  line (width/2,0,width/2,height);
+  line (width/2,0,x,height);
+  stroke (0, 255, 255);
+  line (width/2,height,width-x,0);
+  x+=v*dir;
+  if(x<0 || x>width){dir=-dir;}
 }
