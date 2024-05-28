1f10bcab33ec288d4183a2a0ae46701265c241cf
viclizzio <victorializzio@abacatania.it>
2024-05-28 14:43:48 +0200

    .

---
+++ Esercizio 26/sketch.js
@@ -0,0 +1,22 @@
+var t="ABACATANIA è la migliore accademia di via franchetti";
+
+function setup() { 
+  createCanvas (500,500);
+
+}
+
+function draw() {
+  background (0);
+
+  fill (255);
+  textSize (50);
+  stroke (255,0,0);
+  strokeWeight (3);
+  textAlign (CENTER,CENTER);
+  text (t, 0, 0, width, height);
+
+  strokeWeight (2);
+  stroke (0,255,0);
+  line (width/2, 0, width/2, height);
+  line (0, height/2, width, height/2);
+}
