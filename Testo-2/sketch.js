fdf7c0e3eb668d84e912804a948bbd22c7bb28a9
viclizzio <victorializzio@abacatania.it>
2024-05-28 15:15:03 +0200

    .

---
+++ Testo-2/sketch.js
@@ -0,0 +1,21 @@
+var t="ABACATANIA";
+
+function setup() { 
+  createCanvas (500,500);
+
+}
+
+function draw() {
+  background (0);
+
+  fill (255);
+  textSize (50);
+  stroke (255,0,0);
+  strokeWeight (3);
+  text (t, width/2-textWidth(t)/2, height/2);
+
+  strokeWeight (2);
+  stroke (0,0,255);
+  line (width/2, 0, width/2, height);
+  line (0, height/2, width, height/2);
+}
