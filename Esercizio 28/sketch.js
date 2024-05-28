860e0fc9fe7e1e2940c8a5ecf55e31592e1efbdc
viclizzio <victorializzio@abacatania.it>
2024-05-28 17:14:49 +0200

    .

--- Esercizio 28/sketch.js
+++ Esercizio 28/sketch.js
@@ -1,6 +1,7 @@
 var i;
 var i2;
 var p;
+var c;
 
 function preload(){
     i=loadImage("./assets/gigi.png");
@@ -13,13 +14,18 @@ function setup(){
     i.resize(i.width/2, i.height/2);
     i2.resize(i2.width/2, i2.height/2);
 
-    i.filter(GRAY);
     i.filter(BLUR, 5);
 }
 
 function draw(){
+    c=get(mouseX-1, mouseY-1);
+
     background(0) // per muoverlo con il mouse (i.get(mouseX, mouseY)); //togliere background per gigi infiniti
     image (i,0,0); // per muoverlo con il mouse (i, mouseX, mouseY);
     p=i2.get(mouseX, mouseY, 150, 150);
     image (p, mouseX, mouseY);
+
+    fill (255);
+    textSize(40);
+    text("R: "+red(c)+" - G: "+green(c)+" - B: "+blue(c),30,height-30);
 }
