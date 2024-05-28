32ab264e9ddbd4e24e2ffaa0a2e1f0e5fd6315d6
viclizzio <victorializzio@abacatania.it>
2024-05-28 15:36:33 +0200

    .

--- Testo-2/sketch.js
+++ Testo-2/sketch.js
@@ -1,21 +1,42 @@
 var scrivendo="";
 var scritto="";
 var f;
+var d=1;
+var a=false;
+function preload(){
+    f=loadFont("./assets/testobello.ttf");
 
-function setup() { 
-  createCanvas (500,500);
 }
+function setup(){
+    createCanvas(500,500);
 
-function draw() {
-  background (0);
-
-  fill (255);
-  textSize (30);
-  text ("Inserisci il testo: "+scrivendo, 20, 40);
 }
+function draw(){
+    background(0,50);
+    textFont("Arial");
+    textSize(30);
+    fill(255);
+    text("insersci il testo: "+scrivendo,20, 40);
+    if(a){
+        textFont(f);
+        textSize(d);
+        text(scritto, width/2-textWidth(scritto)/2, height/2);
+        d++;
+        if(textWidth(scritto)>width-10){
+            a=false;
+        }
+    }
+
 
 function keyPressed(){
-  if(keyCode==32 || keyCode>=48){
-    scrivendo+=key;
-  }
+    if(keyCode==32 || keyCode>=48){
+        scrivendo+=key;
+    }
+    if(keyCode==13){
+        scritto=scrivendo;
+        scrivendo="";
+        a=true;
+        d=1;
+    }
+  
 }
