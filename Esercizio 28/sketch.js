163634e2dfa821b7a9b2f0550e2faf27d45ca589
viclizzio <victorializzio@abacatania.it>
2024-05-28 16:38:41 +0200

    .

--- Esercizio 28/sketch.js
+++ Esercizio 28/sketch.js
@@ -1,11 +1,7 @@
-var scrivendo="";
-var scritto="";
-var f;
-var d=1;
-var a=false;
+var i;
 
 function preload(){
-    f=loadFont("./assets/testobello.ttf");
+    i=loadImage("../assets/gigi.png)");
 }
 
 function setup(){
@@ -13,35 +9,6 @@ function setup(){
 }
 
 function draw(){
-    background(0,50);
-
-    textFont("Arial");
-    textSize(30);
-    fill(255);
-    text("Inserisci il testo: " +scrivendo, 20, 40);
-
-    if(a){
-        textFont(f);
-        textSize(d);
-        text(scritto, width/2-textWidth(scritto)/2, height/2);
-        d++;
-        if(textWidth(scritto)>width-10){
-            a=false;
-        }
-    }
-}
-
-function keyPressed(){
-    if(keyCode==32 || keyCode>=48){
-        scrivendo+=key;
-    }
-    if(keyCode==13){
-        scritto=scrivendo;
-        scrivendo="";
-        a=true;
-        d=1;
-    }
-    else if (keyCode==8){
-        scrivendo=scrivendo.substring(0,scrivendo.lenght-1);
-    }
+    background(0);
+    image (i,0,0);
 }
