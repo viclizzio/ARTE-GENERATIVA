78ff1eb6f7aa8f1f518dddb8a1c4a993e38e9768
viclizzio <victorializzio@abacatania.it>
2024-05-28 17:23:55 +0200

    .

--- Esercizio 29/sketch.js
+++ Esercizio 29/sketch.js
@@ -1,7 +1,4 @@
-
-function preload(){
-    
-}
+var a=0;
 
 function setup(){
     createCanvas(500,500);
@@ -9,5 +6,15 @@ function setup(){
 }
 
 function draw(){
-    
+    background (0);
+
+    translate (100,100);
+    rotate (radians(a));
+
+    fill (255,255,0);
+    rect (0,0,50,50);
+   
+    fill (0,255,255);
+    rect (100,0,50,50);
+    a+=3;
 }
