2807e19850164f00436bd98280e2b93d896437eb
viclizzio <victorializzio@abacatania.it>
2024-05-29 22:21:43 +0200

    .

--- Esercizio 32/sketch.js
+++ Esercizio 32/sketch.js
@@ -1,30 +1,49 @@
 var x;
-var v=5;
+var v=10;
 var dir=-1;
 var c=255;
-
-function setup(){
-    createCanvas(600,400);
-    rectMode (CORNERS);
-
-    x=width/2;
+var cont=0;
+var imm;
+function preload(){
+   imm=loadImage("./files/sailor.png");
 }
+function setup(){
+   createCanvas(600,400);
+   rectMode(CORNERS);
+   x=width/2;
 
+}
 function draw(){
-    background (0);
+   background(0);
 
-    fill (c);
-    rect (0,0,x,height);
-    rect (width-x,0,width,height);
-    
-    x+=v*dir;
-    if (x<0 || x>width/2){
-      dir=-dir;  
-    }
+   fill(c);
 
-    if (x>width/2){
-        c=color (random(255), random(255), random(255));
-    }
-    
+   if(cont%4==0){
+      ellipse(width/2, height/2, 60);
+   }
 
+   if(cont%4==1){
+      rect(width/2-30, height/2-30, width/2+30, height/2+30)
+   }
+
+   if(cont%4==2){
+      triangle(width/2, height/2-30, width/2-30, height/2+30, width/2+30, height/2+30);
+   }
+
+   if(cont%4==3){
+      image(imm,0,0,width, height);
+   }
+
+   rect(0,0,x,height);
+   rect(width-x,0,width,height);
+   
+   x+=v*dir;
+   if(x<0 || x>width/2){
+      dir=-dir;
+   }
+   if(x>width/2){
+      cont++;
+      c=color(random(255), random(255), random(255));
+   }
+   
 }
--- Esercizio 34/sketch.js
+++ Esercizio 34/sketch.js
@@ -1,30 +1,51 @@
 var x;
 var y;
-var v;
 var d=50;
 var r=255;
 var g=255;
 var b=0;
 var st=0;
+var v=5;
 
 function setup(){
-    createCanvas(500,500);
-    x = d/2;
-    y = d/2;
+  createCanvas(500,500);
+  x=d/2;
+  y=d/2;
 }
 
 function draw(){
-    background (0);
+  background(255);
+  fill(r,g,b);
+  ellipse(x,y,d,d);
 
-    fill (r,g,b);
-    ellipse (x,y,d,d);
-
-    if(st==0){
-        x+=v;
-        y+=sv;
-        if(x>=width/2){
-            st==1;
-        }
+  if(st==0){
+    x+=v;
+    y+=v;
+    if(x>=width/2){
+      st++;
     }
+  }
 
+  if(st==1){
+    d++;
+    if(d>100){
+      st++;
+    }
+  }
+
+  if(st==2){
+    r--;
+    g--;
+    b++;
+    if(b>255){
+      st++;
+    }
+  }
+  
+  if(st==3){
+    y+=v;
+    if(y>=height-d/2){
+      v=0;
+    }
+  }
 }
