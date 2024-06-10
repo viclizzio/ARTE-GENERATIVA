a349bb28fefd9a34270b0baf597ec4436fe1c1e9
viclizzio <victorializzio@abacatania.it>
2024-06-10 16:12:31 +0200

    -

--- Esercizio 51/sketch.js
+++ Esercizio 51/sketch.js
@@ -1,20 +1,49 @@
 var s;
-var q=25;
-var d=15;
+var q=20;
+
+var r=255;
+var g=255;
+var b=255;
 
 function setup(){
-  createCanvas (700,500);
+  createCanvas (500,500);
 
   s=width/q
 }
 
 function draw(){ 
-  background (0);
+  background (255);
 
   for (var y=0; y<=height; y+=s){
     
     for (var x=0; x<=width; x+=s){
-      ellipse (x+s/2,y+s/2,d,d);
+      fill (r,g,b);
+
+      line (x+s/2,y+s/2, width/2,height/2);
+      ellipse (x+s/2,y+s/2, s/2,s/2);
+
+      if (x<=width/2-s/2 && y<=height/2){
+        r=255;
+        g=0;
+        b=0;
+      }
+
+      if (x<=width/2-s/2 && y>=height/2){
+        r=0;
+        g=255;
+        b=0;
+      }
+
+      if (x>=width/2-s/2 && y>=height/2){
+        r=0;
+        g=0;
+        b=255;
+      }
+      if (x>=width/2-s/2 && y<=height/2){
+        r=255;
+        g=255;
+        b=0;
+      }
     }
   }
 }
